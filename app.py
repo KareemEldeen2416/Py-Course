248044229dfdbb60254fac61894d5ed8373355e6
KareemEldeen Ahmed <kareemeldeenahmed@gmail.com>
2023-10-24 07:41:50 -0700

    Until 10

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 
+# print("I love python")
+# print("I love programming")
+
+# print(type(10))
+# print(type("Kareem"))
+# print(type(1.3))
+# print(type([1, 2, 3]))  # List
+# print([1, 2, 3])
+# print((1, 2, 3))
+# print(type((1, 2, 3)))  # Tuple
+# print({"One": 1, "Two": 2})
+# print(type({"One": 1, "Two": 2}))  # dict -> dictionary
+# print ( 2 == 2)
 
 
+# name = "Kareem"
+# print(name)
+# help("keywords")  # To show reserved words
+# a, b, c = 1, 2, 3
+# print(a, b, c)
 
 
+# print("Hello\bworld")
+# print("Hello\nworld")
+# print("This is a\"Quote\"")
+# print("Fifa 24\rfc  ")
+# print("Python\tLanguage")
+# print("\x4f")
 
 
-
-
-
-
-
-
-
-
-
-
+# msg = "I like"
+# lang = "Python"
+# print(msg + " " + lang)
